9b727dd3482c94a8b0918298eb52cc1bb5ff1189
Artyom Sventetsky <artyom.sven@gmail.com>
2020-08-07 14:07:33 +0300

    init project

---
+++ src/test/java/org/sventetsky/springframework/boot/api_service_logging_spring_boot_starter/ApiServiceLoggingSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sventetsky.springframework.boot.api_service_logging_spring_boot_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceLoggingSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
